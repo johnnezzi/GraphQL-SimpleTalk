d00b87acf05c2a6569611c323a417e29ed4c9d29
John Nezzi <johnnezzi@yahoo.com>
2019-07-14 22:49:28 +0100

    first commit

---
+++ GraphQL-SimpleTalk/Entities/Author.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace GraphQLSimpleTalk.Entities
+{
+    public class Author
+    {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Bio { get; set; }
+            public string ImgUrl { get; set; }
+            public string ProfileUrl { get; set; }
+    }
+}
---
+++ GraphQL-SimpleTalk/Entities/Comment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace GraphQLSimpleTalk.Entities
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
